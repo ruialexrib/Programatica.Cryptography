12b3be9941ca324608e46d2205c6301481749697
Rui Ribeiro <ruialexrib@gmail.com>
2021-08-05 23:12:20 +0100

    initial import

---
+++ src/Programatica.Cryptography/IStringCipherService.cs
@@ -0,0 +1,8 @@
+﻿namespace Programatica.Cryptography
+{
+    public interface IStringCipherService
+    {
+        string Decrypt(string cipherText);
+        string Encrypt(string plainText);
+    }
+}
---
+++ src/Programatica.Cryptography/StringCipher.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace Programatica.Cryptography
+{
+    public class StringCipherService : IStringCipherService
+    {
+        private const string Purpose = "4858055718878509223360720679625487535120";
+        private readonly IDataProtectionProvider _provider;
+
+        public StringCipherService(IDataProtectionProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public string Encrypt(string plainText)
+        {
+            var protector = _provider.CreateProtector(Purpose);
+            return protector.Protect(plainText);
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            var protector = _provider.CreateProtector(Purpose);
+            return protector.Unprotect(cipherText);
+        }
+    }
+}
